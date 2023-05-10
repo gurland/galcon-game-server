b59ed095726097c48a7d3dec9a2052abb56fe617
gurland <stas.gurland@gmail.com>
2023-05-10 19:06:47 +0300

    feat: add default user seeding

--- src/models/data-source.ts
+++ src/models/data-source.ts
@@ -1,19 +1,30 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./User"
+import bcrypt from "bcrypt";
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "database.sqlite",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+  type: "sqlite",
+  database: "database.sqlite",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
 })
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log("Database is initialized");
-    })
-    .catch((error) => console.log(error))
+  .then(async () => {
+    console.log("Database is initialized");
+    try {
+      const newUser = new User()
+      newUser.username = "test";
+      newUser.passwordHash = await bcrypt.hash("test", 10);
+
+      await AppDataSource.manager.save(newUser)
+      console.log("Seeded new test:test user!");
+    } catch (error) {
+      console.log("Skipping seeding test user!");
+    }
+  })
+  .catch((error) => console.log(error))
