e1df4ecc3fa4c5fc4c14f4db20bbed227f6ec2ae
gurland <stas.gurland@gmail.com>
2023-05-26 03:26:16 +0300

    feat: create an initial room connection handler

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,8 +1,22 @@
 import {Socket} from "./events/base";
+import {RoomsManager} from "./entities/rooms_manager";
+import {disconnectSocketWithError} from "./utils";
 
 
 export const handleInitialConnection = (socket: Socket) => {
-  const roomId = socket.data.roomId || 0;
-  socket.join(roomId.toString());
-  socket.emit("RoomUserJoin", {user: socket.data.user!})
+  const room = RoomsManager.getRoomById(socket.data.roomId!);
+  if (!room)
+    return disconnectSocketWithError(socket, `Room with id ${socket.data.roomId} was not found!`)
+
+  console.log(room.getUserById(socket.data.user!.id));
+
+  if (room.getUserById(socket.data.user!.id)) {
+    return disconnectSocketWithError(socket, `User is already in room with id ${room.id}`)
+  }
+
+  else {
+    socket.join(socket.data.roomId!.toString())
+    room.addUser(socket.data.user!)
+    room.users.forEach(user => socket.emit("RoomUserJoin", {user}))
+  }
 }
