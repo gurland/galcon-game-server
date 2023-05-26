0b29c382a3f928e9f880b7a0b2dc61debf3d272c
gurland <stas.gurland@gmail.com>
2023-05-26 03:25:15 +0300

    feat: add helper to disconnect a socket with an ErrorEvent

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
+import {Socket} from "./events/base";
+
 export function getRandomFloat(leftBound: number, rightBound: number): number {
   return Math.random() * (rightBound - leftBound) + leftBound;
+}
+
+export function disconnectSocketWithError(socket: Socket, errorMessage: string): void {
+  socket.emit("ErrorEvent", {"message": errorMessage});
+  socket.disconnect();
 }
